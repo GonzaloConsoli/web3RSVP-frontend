334c69107e973bc87cccc5a675cab4aba58a77c4
Gonzalo Consoli <gconsoli@mpf.gov.ar>
2023-01-07 01:05:46 -0300

    more actions

--- pages/my-events/upcoming.js
+++ pages/my-events/upcoming.js
@@ -5,74 +5,73 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
 import EventCard from "../../components/EventCard";
 
-const MY_UPCOMING_RSVPS = gql`
-  query Account($id: String) {
-    account(id: $id) {
+const MY_UPCOMING_EVENTS = gql`
+  query Events($eventOwner: String, $currentTimestamp: String) {
+    events(
+      where: { eventOwner: $eventOwner, eventTimestamp_gt: $currentTimestamp }
+    ) {
       id
-      rsvps {
-        event {
-          id
-          name
-          eventTimestamp
-          imageURL
-        }
-      }
+      eventID
+      name
+      description
+      eventTimestamp
+      maxCapacity
+      totalRSVPs
+      imageURL
     }
   }
 `;
 
-export default function MyUpcomingRSVPs() {
+export default function MyUpcomingEvents() {
   const { data: account } = useAccount();
 
-  const id = account ? account.address.toLowerCase() : "";
-  const [currentTimestamp, setEventTimestamp] = useState(new Date().getTime());
-  const { loading, error, data } = useQuery(MY_UPCOMING_RSVPS, {
-    variables: { id },
+  const eventOwner = account ? account.address.toLowerCase() : "";
+  const [currentTimestamp, setEventTimestamp] = useState(
+    new Date().getTime().toString()
+  );
+  const { loading, error, data } = useQuery(MY_UPCOMING_EVENTS, {
+    variables: { eventOwner, currentTimestamp },
   });
 
   if (loading)
     return (
-      <Dashboard page="rsvps" isUpcoming={true}>
+      <Dashboard page="events" isUpcoming={true}>
         <p>Loading...</p>
       </Dashboard>
     );
   if (error)
     return (
-      <Dashboard page="rsvps" isUpcoming={true}>
+      <Dashboard page="events" isUpcoming={true}>
         <p>`Error! ${error.message}`</p>
       </Dashboard>
     );
 
   return (
-    <Dashboard page="rsvps" isUpcoming={true}>
+    <Dashboard page="events" isUpcoming={true}>
       {account ? (
         <div>
-          {data && !data.account && <p>No upcoming RSVPs found</p>}
-          {data && data.account && (
+          {data && data.events.length == 0 && <p>No upcoming events found</p>}
+          {data && data.events.length > 0 && (
             <ul
               role="list"
               className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
             >
-              {data.account.rsvps.map(function (rsvp) {
-                if (rsvp.event.eventTimestamp > currentTimestamp) {
-                  return (
-                    <li key={rsvp.event.id}>
-                      <EventCard
-                        id={rsvp.event.id}
-                        name={rsvp.event.name}
-                        eventTimestamp={rsvp.event.eventTimestamp}
-                        imageURL={rsvp.event.imageURL}
-                      />
-                    </li>
-                  );
-                }
-              })}
+              {data.events.map((event) => (
+                <li key={event.id}>
+                  <EventCard
+                    id={event.id}
+                    name={event.name}
+                    eventTimestamp={event.eventTimestamp}
+                    imageURL={event.imageURL}
+                  />
+                </li>
+              ))}
             </ul>
           )}
         </div>
       ) : (
         <div className="flex flex-col items-center py-8">
-          <p className="mb-4">Please connect your wallet to view your rsvps</p>
+          <p className="mb-4">Please connect your wallet to view your events</p>
           <ConnectButton />
         </div>
       )}
